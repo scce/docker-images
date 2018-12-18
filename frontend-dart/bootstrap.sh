175e0cc357b2d2598e63dcc58b81bbb515496a81
zweihoff <philip.zweihoff@cs.uni-dortmund.de>
2018-12-18 10:23:18 +0100

    enabled dart 2.1 for frontend

--- frontend-dart/bootstrap.sh
+++ frontend-dart/bootstrap.sh
@@ -2,14 +2,15 @@
 case "$1" in
   "-local")
     pub get
-    pub serve --hostname=0.0.0.0 --define=local=true
+    export local=true
+    /root/.pub-cache/bin/webdev serve --hostname=0.0.0.0
     ;;
   "-production")
     cp -r /webapp/* /webbuild/
     cd /webbuild
     pub get
-    pub build --define=local=false
-    cp -r ./build/web/* /usr/share/nginx/html/
+    /root/.pub-cache/bin/webdev build
+    cp -r ./build/* /usr/share/nginx/html/
     ;;
   *)
     echo "Please specify the mode: [\"-local\", \"-production\"]"
